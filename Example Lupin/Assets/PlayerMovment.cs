23e27195a6b7bcd9f45a46ee0b74ffb8d09403c1
Ananda Johnson <Sovember@gmail.com>
2024-03-17 23:39:35 -0500

    More math

--- Example Lupin/Assets/PlayerMovment.cs
+++ Example Lupin/Assets/PlayerMovment.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
+
+    Rigidbody rb;
+    int wholeNumber = 3;
+    float decimalNumber = 3.45f;
+
     // Start is called before the first frame update
     void Start()
     {
-    
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -15,16 +20,27 @@ public class PlayerMovment : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0);
+            rb.velocity = new Vector3(0, 5, 0);
         }
 
         if (Input.GetKey("up"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+            rb.velocity = new Vector3(0, 0, 5);
         }
+
+        if (Input.GetKey("down"))
+        {
+            rb.velocity = new Vector3(0, 0, -5);
+        }
+
         if (Input.GetKey("right"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(5, 0, 0);
+            rb.velocity = new Vector3(5, 0, 0);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            rb.velocity = new Vector3(-5, 0, 0);
         }
     }
 }
