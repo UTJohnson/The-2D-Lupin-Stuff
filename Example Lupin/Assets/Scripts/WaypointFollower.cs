52aa6364e2995d815b8edaa21507ae8b5989a24f
Ananda Johnson <ananda@paragondelta.com>
2024-06-06 07:23:47 -0500

    Latest

---
+++ Example Lupin/Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollower : MonoBehaviour
+{
+    [SerializeField] GameObject[] waypoints;
+    int currentWaypointIndex = 0;
+
+    [SerializeField] float speed = 1f;
+    void Update()
+    {
+        
+    }
+}
