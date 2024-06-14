a12adc2f51b94797c0b6dffd39f8bcea1a1bb922
Ananda Johnson <ananda@paragondelta.com>
2024-06-14 03:29:01 -0500

    Waypoint script
    Add WaypointFollower script for moving platform. Enable full solution analysis for comprehensive diagnostics in Visual Studio.

--- Example Lupin/Assets/Scripts/WaypointFollower.cs
+++ Example Lupin/Assets/Scripts/WaypointFollower.cs
@@ -2,6 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * WaypointFollower is a script for Unity that enables an object to follow a series of waypoints.
+ * 
+ * Variables:
+ * - waypoints: An array of GameObjects representing the waypoints that the object will follow.
+ * - currentWaypointIndex: An integer tracking the current waypoint the object is moving towards.
+ * - speed: A float determining the speed at which the object moves towards the waypoint.
+ * 
+ * Methods:
+ * - Update: Called once per frame, this method moves the object towards the current waypoint. 
+ *   When the object is close enough to the current waypoint (within 0.1 units), it increments the waypoint index.
+ *   If the index exceeds the length of the waypoints array, it loops back to the start.
+ *   The object then moves towards the new current waypoint.
+ */
+
+
 public class WaypointFollower : MonoBehaviour
 {
     [SerializeField] GameObject[] waypoints;
@@ -11,6 +27,17 @@ public class WaypointFollower : MonoBehaviour
 
     void Update()
     {
+
+        if(Vector3.Distance(transform.position, waypoints[currentWaypointIndex].transform.position) < .1f)
+        { 
+            currentWaypointIndex++;
+            if(currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        
+        }
+
         transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime);
     }
 }
