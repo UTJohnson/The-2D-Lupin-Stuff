e04a689e95a6916e2e5b044341b9182e430f57b9
Ananda Johnson <ananda@paragondelta.com>
2024-08-16 22:21:56 -0500

    Massive Update
    Massive Update.

---
+++ Example Lupin/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+
+    bool dead = false;
+    private void Update() {
+        if(transform.position.y < -1f && !dead) 
+        {
+            Die();
+        }
+    }
+    // Start is called before the first frame update
+   private void OnCollisionEnter(Collision collision) {
+    if (collision.gameObject.CompareTag("Enemy Body")) {
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<PlayerMovment>().enabled = false;
+        Die();
+    }
+   }
+
+   void Die(){
+      Invoke(nameof(ReloadLevel), 1.3f);
+      dead = true;
+      Debug.Log("ded!");
+
+   }
+
+   void ReloadLevel() {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+   }
+}
